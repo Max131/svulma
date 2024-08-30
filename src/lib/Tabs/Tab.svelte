956b352e9b8f5c2778c3e81c27bc0c6d15c29a25
Mario <31197372+Max131@users.noreply.github.com>
2024-08-30 00:24:49 -0600

    style: fix single quotes

--- src/lib/Tabs/Tab.svelte
+++ src/lib/Tabs/Tab.svelte
@@ -1,58 +1,58 @@
 <script context="module">
-	/**
-	 * Set the tab id number
-	 * @type {number}
-	 */
-	let tabID = 0;
+  /**
+   * Set the tab id number
+   * @type {number}
+   */
+  let tabID = 0;
 </script>
 
 <script>
-	import { getContext } from 'svelte';
-	import { fade } from 'svelte/transition';
+  import { getContext } from "svelte";
+  import { fade } from "svelte/transition";
 
-	/**
-	 * The label displayed on the tab.
-	 * @type {string}
-	 * @default "Tab"
-	 */
-	export let label = 'Tab';
+  /**
+   * The label displayed on the tab.
+   * @type {string}
+   * @default "Tab"
+   */
+  export let label = "Tab";
 
-	/**
-	 * Whether the tab is currently active.
-	 * @type {boolean}
-	 * @default false
-	 */
-	export let active = false;
+  /**
+   * Whether the tab is currently active.
+   * @type {boolean}
+   * @default false
+   */
+  export let active = false;
 
-	/**
-	 * The icon displayed on the tab, which can be a string (e.g., an icon name) or a function that returns a Svelte component.
-	 * @type {string | function(): SvelteComponent}
-	 * @default ""
-	 */
-	export let icon = '';
+  /**
+   * The icon displayed on the tab, which can be a string (e.g., an icon name) or a function that returns a Svelte component.
+   * @type {string | function(): SvelteComponent}
+   * @default ""
+   */
+  export let icon = "";
 
-	/**
-	 * Properties for the icon component, if any.
-	 * @type {Object}
-	 * @default {}
-	 */
-	export let iconProps = {};
+  /**
+   * Properties for the icon component, if any.
+   * @type {Object}
+   * @default {}
+   */
+  export let iconProps = {};
 
-	const { registerTab, currentTab, setActiveTab } = getContext('manageTabs');
-	const id = `tab-${tabID++}`;
+  const { registerTab, currentTab, setActiveTab } = getContext("manageTabs");
+  const id = `tab-${tabID++}`;
 
-	registerTab({ id, label, icon, iconProps, active });
+  registerTab({ id, label, icon, iconProps, active });
 
-	// $: if (active) {
-	// 	$currentTab.id = id;
-	// 	$currentTab.label = label;
-	// }
+  // $: if (active) {
+  // 	$currentTab.id = id;
+  // 	$currentTab.label = label;
+  // }
 
-	$: active && setActiveTab({ id, label });
+  $: active && setActiveTab({ id, label });
 </script>
 
 {#if $currentTab.id === id}
-	<div class="tab-panel" role="tabpanel" aria-labelledby={id} tabindex="0" in:fade>
-		<slot />
-	</div>
+  <div class="tab-panel" role="tabpanel" aria-labelledby={id} tabindex="0" in:fade>
+    <slot />
+  </div>
 {/if}
--- src/lib/Tabs/Tabs.svelte
+++ src/lib/Tabs/Tabs.svelte
@@ -1,119 +1,119 @@
 <script>
-	import { setContext, createEventDispatcher } from 'svelte';
-	import { writable } from 'svelte/store';
-	import Icon from '$lib/Icon';
+  import { setContext, createEventDispatcher } from "svelte";
+  import { writable } from "svelte/store";
+  import Icon from "$lib/Icon";
 
-	/**
-	 * Alignment of the tabs: can be `is-left`, `is-center`, `is-right`.
-	 * @type {'is-center' | 'is-left' | 'is-right'}
-	 * @default "is-left"
-	 */
-	export let align = '';
+  /**
+   * Alignment of the tabs: can be `is-left`, `is-center`, `is-right`.
+   * @type {'is-center' | 'is-left' | 'is-right'}
+   * @default "is-left"
+   */
+  export let align = "";
 
-	/**
-	 * Size of the tabs: can be `is-small`, `is-medium`, or `is-large`.
-	 * @type {'is-small' | 'is-medium' | 'is-large'}
-	 * @default "is-medium"
-	 */
-	export let size = '';
+  /**
+   * Size of the tabs: can be `is-small`, `is-medium`, or `is-large`.
+   * @type {'is-small' | 'is-medium' | 'is-large'}
+   * @default "is-medium"
+   */
+  export let size = "";
 
-	/**
-	 * Style of the tabs: can be `is-boxed` or `is-toggle`.
-	 * @type {'is-boxed' | 'is-toggle'}
-	 * @default ""
-	 */
-	export let style = '';
+  /**
+   * Style of the tabs: can be `is-boxed` or `is-toggle`.
+   * @type {'is-boxed' | 'is-toggle'}
+   * @default ""
+   */
+  export let style = "";
 
-	/**
-	 * If the toggle style should be rounded.
-	 * @type {boolean}
-	 * @default false
-	 */
-	export let roundedToggle = false;
+  /**
+   * If the toggle style should be rounded.
+   * @type {boolean}
+   * @default false
+   */
+  export let roundedToggle = false;
 
-	/**
-	 * If the tabs should be full width.
-	 * @type {boolean}
-	 * @default false
-	 */
-	export let fullwidth = false;
+  /**
+   * If the tabs should be full width.
+   * @type {boolean}
+   * @default false
+   */
+  export let fullwidth = false;
 
-	// Event dispatcher for custom events
-	const dispatch = createEventDispatcher();
+  // Event dispatcher for custom events
+  const dispatch = createEventDispatcher();
 
-	/**
-	 * Store that keeps track of the currently active tab.
-	 * @type {import('svelte/store').Writable<{id: string, label: string}>}
-	 */
-	const currentTab = writable({});
+  /**
+   * Store that keeps track of the currently active tab.
+   * @type {import('svelte/store').Writable<{id: string, label: string}>}
+   */
+  const currentTab = writable({});
 
-	// Array to store the registered tabs
-	let tabs = [];
+  // Array to store the registered tabs
+  let tabs = [];
 
-	/**
-	 * Function to register a tab. Adds the tab to the internal list of tabs.
-	 * @param {{id: string, label: string, icon?: string | function(): SvelteComponent, iconProps?: Object, active?: boolean}} tab - The tab to register.
-	 */
-	const registerTab = (tab) => {
-		if (!$currentTab.id) {
-			$currentTab = { ...tab };
-		}
+  /**
+   * Function to register a tab. Adds the tab to the internal list of tabs.
+   * @param {{id: string, label: string, icon?: string | function(): SvelteComponent, iconProps?: Object, active?: boolean}} tab - The tab to register.
+   */
+  const registerTab = (tab) => {
+    if (!$currentTab.id) {
+      $currentTab = { ...tab };
+    }
 
-		tabs = [...tabs, tab];
-	};
+    tabs = [...tabs, tab];
+  };
 
-	/**
-	 * Function to set the active tab.
-	 * @param {{id: string, label: string}} tab - The tab to set as active.
-	 */
-	const setActiveTab = ({ id, label }) => {
-		// Update the current tab store with the new active tab
-		$currentTab = { id, label };
+  /**
+   * Function to set the active tab.
+   * @param {{id: string, label: string}} tab - The tab to set as active.
+   */
+  const setActiveTab = ({ id, label }) => {
+    // Update the current tab store with the new active tab
+    $currentTab = { id, label };
 
-		// Dispatch a 'change' event with the updated active tab
-		dispatch('change', {
-			activeTab: $currentTab
-		});
-	};
+    // Dispatch a 'change' event with the updated active tab
+    dispatch("change", {
+      activeTab: $currentTab
+    });
+  };
 
-	/*
-	 * Function to set the active tab with keyboard space and enter
-	 * @param {{id: string, label: string}} tab - The tab to set as active.
-	 * @param {string} code - The code from event
-	 */
-	const setActiveTabWithKeyboard = (code, { id, label }) => {
-		if (code === 'Enter' || code === 'Space' || code === 'NumpadEnter') {
-			setActiveTab({ id, label });
-		}
-	};
+  /*
+   * Function to set the active tab with keyboard space and enter
+   * @param {{id: string, label: string}} tab - The tab to set as active.
+   * @param {string} code - The code from event
+   */
+  const setActiveTabWithKeyboard = (code, { id, label }) => {
+    if (code === "Enter" || code === "Space" || code === "NumpadEnter") {
+      setActiveTab({ id, label });
+    }
+  };
 
-	/**
-	 * Function to set the active tab with keyboard arrows
-	 * @param {{target: listElement, code: string}} event
-	 */
-	const selectSiblingTab = ({ target, code }) => {
-		const listElement = target.closest('li');
+  /**
+   * Function to set the active tab with keyboard arrows
+   * @param {{target: listElement, code: string}} event
+   */
+  const selectSiblingTab = ({ target, code }) => {
+    const listElement = target.closest("li");
 
-		if (listElement && code === 'ArrowLeft' && listElement.previousElementSibling) {
-			const tabButton = listElement.previousElementSibling.firstElementChild;
-			tabButton.focus();
-			tabButton.click();
-			return;
-		}
+    if (listElement && code === "ArrowLeft" && listElement.previousElementSibling) {
+      const tabButton = listElement.previousElementSibling.firstElementChild;
+      tabButton.focus();
+      tabButton.click();
+      return;
+    }
 
-		if (listElement && code === 'ArrowRight' && listElement.nextElementSibling) {
-			const tabButton = listElement.nextElementSibling.firstElementChild;
-			tabButton.focus();
-			tabButton.click();
-			return;
-		}
-	};
+    if (listElement && code === "ArrowRight" && listElement.nextElementSibling) {
+      const tabButton = listElement.nextElementSibling.firstElementChild;
+      tabButton.focus();
+      tabButton.click();
+      return;
+    }
+  };
 
-	// Provide the tab management functions and state to child components
-	setContext('manageTabs', { registerTab, currentTab, setActiveTab });
+  // Provide the tab management functions and state to child components
+  setContext("manageTabs", { registerTab, currentTab, setActiveTab });
 
-	// Uncomment this block if you want to set a default active tab on mount
-	/* onMount(() => {
+  // Uncomment this block if you want to set a default active tab on mount
+  /* onMount(() => {
         const existActiveTab = tabs.every((tab) => {
             return tab.active;
         });
@@ -126,43 +126,43 @@
 </script>
 
 <div
-	class="tabs {style} {align} {size}"
-	class:is-toggle-rounded={roundedToggle}
-	class:is-fullwidth={fullwidth}
-	id={crypto.randomUUID()}
+  class="tabs {style} {align} {size}"
+  class:is-toggle-rounded={roundedToggle}
+  class:is-fullwidth={fullwidth}
+  id={crypto.randomUUID()}
 >
-	<ul role="tablist">
-		{#each tabs as { label, icon, iconProps, id } (id)}
-			<li data-tab={id} class:is-active={$currentTab.id === id}>
-				<a
-					on:click|preventDefault={() => setActiveTab({ id, label })}
-					on:keyup={({ code }) => setActiveTabWithKeyboard(code, { id, label })}
-					on:keyup={selectSiblingTab}
-					role="tab"
-					tabindex="0"
-					{id}
-					aria-selected={$currentTab.id === id}
-				>
-					{#if icon}
-						<!-- Render the Icon component if `icon` is provided -->
-						<Icon {icon} {iconProps} />
-					{/if}
-					<span>
-						{label}
-					</span>
-				</a>
-			</li>
-		{/each}
-	</ul>
+  <ul role="tablist">
+    {#each tabs as { label, icon, iconProps, id } (id)}
+      <li data-tab={id} class:is-active={$currentTab.id === id}>
+        <a
+          on:click|preventDefault={() => setActiveTab({ id, label })}
+          on:keyup={({ code }) => setActiveTabWithKeyboard(code, { id, label })}
+          on:keyup={selectSiblingTab}
+          role="tab"
+          tabindex="0"
+          {id}
+          aria-selected={$currentTab.id === id}
+        >
+          {#if icon}
+            <!-- Render the Icon component if `icon` is provided -->
+            <Icon {icon} {iconProps} />
+          {/if}
+          <span>
+            {label}
+          </span>
+        </a>
+      </li>
+    {/each}
+  </ul>
 </div>
 
 <div class="tabs-content">
-	<slot />
+  <slot />
 </div>
 
 <style>
-	a:focus-visible {
-		outline-offset: -2px;
-		border-radius: 4px;
-	}
+  a:focus-visible {
+    outline-offset: -2px;
+    border-radius: 4px;
+  }
 </style>
