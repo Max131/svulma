c3e7528b913fd73b37c9a974d78c9a02f3299636
Mario <31197372+Max131@users.noreply.github.com>
2024-09-05 00:12:12 -0600

    feat(core): add main exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,6 @@
-// place files you want to import through the `$lib` alias in this folder.
+import { Tabs, Tab } from "./Tabs";
+import { Modal, ModalCard } from "./Modal";
+import { Field } from "./Form";
+import Icon from "./Icon";
+
+export { Tabs, Tab, Modal, ModalCard, Field, Icon };
