eb67886e1f8db686bfee1cbd4a5022d0aa7dc516
Mario <31197372+Max131@users.noreply.github.com>
2024-09-07 15:29:07 -0600

    fix(core): import/export Input

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,6 @@
 import { Tabs, Tab } from "./Tabs";
 import { Modal, ModalCard } from "./Modal";
-import { Field } from "./Form";
+import { Field, Input } from "./Form";
 import Icon from "./Icon";
 
-export { Tabs, Tab, Modal, ModalCard, Field, Icon };
+export { Tabs, Tab, Modal, ModalCard, Field, Input, Icon };
